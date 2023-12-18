cad2cd082150b08c7aa69c98a33b29e4ba565d4d
s0lh4ck <154092454+s0lh4ck@users.noreply.github.com>
2023-12-18 19:05:53 +0100

    Update ttlosinspector.py

--- ttlosinspector.py
+++ ttlosinspector.py
@@ -17,24 +17,24 @@ def detect_operating_system(ip_address):
 
     try:
         if operating_system == "windows":
-            ping_result = subprocess.check_output(["ping", "-n", "1", ip_address])
+            ping_result = subprocess.check_output(["ping", "-n", "1", ip_address], text=True, stderr=subprocess.STDOUT)
         elif operating_system == "linux":
-            ping_result = subprocess.check_output(["ping", "-c", "1", ip_address])
+            ping_result = subprocess.check_output(["ping", "-c", "1", ip_address], text=True, stderr=subprocess.STDOUT)
         else:
             print(f"Unsupported operating system: {operating_system}")
             return
 
-        ttl_match = re.search(r"TTL=(\d+)|time=(\d+)", ping_result.decode('latin-1'))
-        
+        ttl_match = re.search(r"ttl=(\d+)|time=(\d+)", ping_result, re.IGNORECASE)
+
         if ttl_match:
             ttl_value = next((group for group in ttl_match.groups() if group is not None), None)
             ttl = int(ttl_value)
-            
+
             if 0 < ttl <= 64:
-                detected_os = platform.system()
+                detected_os = "Linux"
                 print(f"Detected operating system: {detected_os}")
             elif 64 < ttl <= 128:
-                detected_os = platform.system()
+                detected_os = "Windows"
                 print(f"Detected operating system: {detected_os}")
             else:
                 print(f"Failed to determine operating system with TTL: {ttl}")
